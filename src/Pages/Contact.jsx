9def7bcd790f397dd6381f050d258846c42824cc
Aarzoo Khan <71270449+AarzooKhan7@users.noreply.github.com>
2022-06-13 10:51:46 +0000

    Realtime

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function Contact() {
+  const [name, setName] = useState(' ');
+  const [btnName, setBtnName] = useState(' ')
+  const inputValue = (e) => {
 
+    setName(e.target.value)
+
+
+  }
+  const onSubmit = () => {
+    setBtnName(name)
+  }
   return (
     <>
+
+      <h1 className="my-4 mx-3">{btnName}</h1>
+
+      <input onChange={inputValue} value={name} type="text" className="form-control my-4 mx-3" style={{ width: "90%", margin: "auto" }} />
+
+      <button className="btn btn-primary my-4 mx-3" onClick={onSubmit}>Submit</button>
 
     </>
 
